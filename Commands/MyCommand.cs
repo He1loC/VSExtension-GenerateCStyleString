e62e357bfbef253820ab1a0152097fa0d1fafd29
he1loc <he1loc@foxmail.com>
2023-03-15 18:58:45 +0800

    Add project files.

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace GenerateCStyleString
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+            if (selection.HasValue) 
+            {
+                var text = selection.Value.GetText();
+                var cstring = "char sz" + text + "[] = {";
+                foreach (var ch in text)
+                {
+                    cstring += $"'{ch}',";
+                }
+                cstring += "'\\0'};";
+                docView.TextBuffer.Replace(selection.Value, cstring);
+
+            }
+        }
+    }
+}
